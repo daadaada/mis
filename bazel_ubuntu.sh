760e99de231d5833269e24d1bd767ebed2c2bc7d
daadaada <dyanab@ust.hk>
2018-05-19 19:59:38 +0800

    Update bazel_ubuntu.sh

--- bazel_ubuntu.sh
+++ bazel_ubuntu.sh
@@ -6,8 +6,8 @@
 sudo apt-get install openjdk-8-jdk
 
 # On Ubuntu 14.04 LTS you must use a PPA:
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update && sudo apt-get install oracle-java8-installer
+# sudo add-apt-repository ppa:webupd8team/java
+# sudo apt-get update && sudo apt-get install oracle-java8-installer
 
 # Add Bazel distribution URI as a package source
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
